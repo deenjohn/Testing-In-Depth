8e33cd396d24628303adfaaf0f06faaaba010d1e
Deen John <dj.itbhu@gmail.com>
2018-04-02 18:12:01 +0530

    Update jest.js

--- jest.js
+++ jest.js
@@ -229,6 +229,27 @@ test('returns winner', () => {
 
 
 
+//mock.test4.js
+
+import thumbWar from '../thumb-war';
+import * as utils from '../utils';
+
+test('returns winner', () => {
+  jest.spyOn(utils, 'getWinner');
+
+  utils.getWinner.mockImplementation((p1, p2) => p2);
+
+  const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds');
+
+  expect(winner).toBe('Kent C. Dodds');
+  utils.getWinner.mockRestore();
+});
+
+
+
+
+
+
 
 
 
