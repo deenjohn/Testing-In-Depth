6d59d1d455d666e8e7b87be0986b3d1de82d86db
Deen John <dj.itbhu@gmail.com>
2018-04-04 21:42:41 +0530

    Update jest2.js

--- jest2.js
+++ jest2.js
@@ -4,6 +4,7 @@
 // https://facebook.github.io/jest/docs/en/tutorial-async.html
 // https://facebook.github.io/jest/docs/en/jest-object.html#jestspyonobject-methodname
 
+//https://medium.com/@boriscoder/the-hidden-power-of-jest-matchers-f3d86d8101b0
 
 // component to be tested
 
